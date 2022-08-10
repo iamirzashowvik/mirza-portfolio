811ec62ecd1e86bf4c8d6ac49f968ecbab82b5a2
mirzashowvik <showvikmirza@gmail.com>
2022-08-10 21:18:56 +0600

    fd

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [
-    './src/**/*.{js,ts,jsx,tsx}',
-    // Add more here
-  ],
   darkMode: 'class',
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
